f16d2f21f1369878db679ad96c92e09fcb6a5e92
Joe Elliott <jelliott@qsrautomations.com>
2018-12-17 09:43:39 -0500

    Copy k8s SA secrets to the expected location

--- .debug/runDelve.sh
+++ .debug/runDelve.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+K8S_SA_PATH=/var/run/secrets/kubernetes.io/serviceaccount
+
+mkdir -p $K8S_SA_PATH
+cp $TELEPRESENCE_ROOT$K8S_SECRETS_PATH/* $K8S_SA_PATH
+
 dlv debug --listen=0.0.0.0:2345 --headless=true --log=true --api-version 2
