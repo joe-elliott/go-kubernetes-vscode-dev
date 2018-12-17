1205e58d6fbde84ab5ca70897ac1d2a8afd259e8
Joe Elliott <jelliott@qsrautomations.com>
2018-12-17 13:46:23 -0500

    Added some basic k8s functionality

--- main.go
+++ main.go
@@ -3,27 +3,45 @@ package main
 import (
 	"os"
 
-	"github.com/golang/glog"
+	logging "github.com/op/go-logging"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
+var (
+	_log       = logging.MustGetLogger("prometheus-autoscaler")
+	_logFormat = logging.MustStringFormatter(
+		`%{time:15:04:05.000} %{level:.4s} %{message}`,
+	)
+)
+
+func init() {
+	backend := logging.NewLogBackend(os.Stdout, "", 0)
+	backendFormatted := logging.NewBackendFormatter(backend, _logFormat)
+
+	logging.SetBackend(backendFormatted)
+}
+
 func main() {
-	glog.Info("Debug Application Staring")
+	_log.Infof("Application Starting")
 
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		glog.Fatal("rest.InClusterConfig failed: ", err)
-		os.Exit(1)
+		_log.Fatalf("rest.InClusterConfig failed: %v", err)
 	}
 	// creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		glog.Fatal("kubernetes.NewForConfig failed: ", err)
-		os.Exit(1)
+		_log.Fatalf("kubernetes.NewForConfig failed: %v", err)
 	}
 
-	glog.Info("clientset: ", clientset)
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		_log.Fatalf("list pods failed: %v", err)
+	}
+
+	_log.Infof("There are %d pods in the cluster", len(pods.Items))
 }
