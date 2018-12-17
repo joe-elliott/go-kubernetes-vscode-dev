c993b0263cdeb4a5af42aa5a7cd77cc38399b0cd
Joe Elliott <jelliott@qsrautomations.com>
2018-12-17 09:45:10 -0500

    Fixed path

--- .debug/runDelve.sh
+++ .debug/runDelve.sh
@@ -3,6 +3,6 @@
 K8S_SA_PATH=/var/run/secrets/kubernetes.io/serviceaccount
 
 mkdir -p $K8S_SA_PATH
-cp $TELEPRESENCE_ROOT$K8S_SECRETS_PATH/* $K8S_SA_PATH
+cp $TELEPRESENCE_ROOT$K8S_SA_PATH/* $K8S_SA_PATH
 
-dlv debug --listen=0.0.0.0:2345 --headless=true --log=true --api-version 2
+dlv debug --listen=0.0.0.0:2345 --headless=true --api-version 2
